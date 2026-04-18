11bce1b62bf3c4c00b1bf2a6f0ad02ef4306ad37
styy_ <le4.tka@yandex.ru>
2026-04-18 19:25:02 +0200

    fix(3): remove dead client mixins

--- mod/src/main/java/com/example/mixin/CustomPayloadMixin.java
+++
@@ -1,8 +0,0 @@
-package com.example.mixin;
-
-import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(CustomPayloadS2CPacket.class)
-public class CustomPayloadMixin {
-}
