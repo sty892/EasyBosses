e12a828ae5e6d64709b16186db7d517138ac4fb6
styy_ <le4.tka@yandex.ru>
2026-04-18 14:19:59 +0200

    fix(mod): selective ArmorStand rendering in BossReplacedRenderer

--- mod/src/main/java/com/example/render/BossReplacedRenderer.java
+++ mod/src/main/java/com/example/render/BossReplacedRenderer.java
@@ -1,8 +1,12 @@
 package com.example.render;
 
 import com.example.anim.BossAnimController;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.ArmorStandEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.nbt.NbtCompound;
 import software.bernie.geckolib.animatable.GeoReplacedEntity;
 import software.bernie.geckolib.renderer.GeoReplacedEntityRenderer;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -10,9 +14,34 @@ import software.bernie.geckolib.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class BossReplacedRenderer extends GeoReplacedEntityRenderer<ArmorStandEntity, BossReplacedRenderer.DummyAnimatable> {
+    private final ArmorStandEntityRenderer vanillaRenderer;
 
     public BossReplacedRenderer(EntityRendererFactory.Context renderManager) {
         super(renderManager, new BossGeoModel(), new DummyAnimatable());
+        this.vanillaRenderer = new ArmorStandEntityRenderer(renderManager);
+    }
+
+    @Override
+    public void render(ArmorStandEntity entity, float entityYaw, float partialTick, MatrixStack poseStack, VertexConsumerProvider bufferSource, int packedLight) {
+        if (isBoss(entity)) {
+            super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+        } else {
+            this.vanillaRenderer.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+        }
+    }
+
+    private boolean isBoss(ArmorStandEntity entity) {
+        if (entity.hasCustomName() && entity.getCustomName().getString().startsWith("[BF:")) {
+            return true;
+        }
+        // PDC tags are synced as NBT in PublicBukkitValues
+        NbtCompound nbt = new NbtCompound();
+        entity.writeCustomDataToNbt(nbt);
+        if (nbt.contains("PublicBukkitValues")) {
+            NbtCompound bukkitNbt = nbt.getCompound("PublicBukkitValues");
+            return bukkitNbt.contains("bossframework:boss_id");
+        }
+        return false;
     }
 
     public static class DummyAnimatable implements GeoReplacedEntity {
