f1d63073ed8633e53967e0ce9ddd57c731e02467
styy_ <le4.tka@yandex.ru>
2026-04-18 03:32:46 +0200

    chore: setup project structure for plugin and mod

---
+++ mod/src/main/java/com/example/BossFrameworkMod.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BossFrameworkMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("BossFramework mod initialized");
+    }
+}
---
+++ plugin/src/main/java/com/example/BossFrameworkPlugin.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BossFrameworkPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("BossFramework plugin enabled!");
+    }
+}
